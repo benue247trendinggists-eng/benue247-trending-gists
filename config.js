4dbc17ca770a873f66c305cad793641a842666db
benue247trendinggists-eng <benue247trendinggists@gmail.com>
2026-08-22 21:29:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-const SUPABASE_URL = "https://zvhbtaokljksjamkxwon.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://zvhbtaokljksjamkxwon.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_YF-IQhjeGymnEDsX-X2BEQ_Ul9-st_m";
-
